87c5c7f47333cb63f67756ba84bd9eeaafda9611
Thanasis Giannelos <athgiannelos@gmail.com>
2022-05-13 00:10:56 +0300

    fix Header's image issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import "./Header.css";
 import SearchBar from "./SearchBar";
+import store_image from "../assets/onlineorinstore.png"
 
 const Header = () => {
   return (
     <header>
+      <img src={store_image} alt=""/>
       <SearchBar/>
     </header>
   );
